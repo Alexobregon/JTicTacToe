00ebad495cfa42fd791050d33eca455b007caa26
Alexander Obregon <alexander.obregon47@gmail.com>
2023-01-24 16:41:09 -0800

    GameBoaed function cleanup

--- TicTacToe.java
+++ TicTacToe.java
@@ -9,13 +9,18 @@ public class TicTacToe {
 				                 {'-', '+', '-', '+', '-'},
 				                 {' ', '|', ' ', '|', ' '}};
 
-    for(char[] row : gameBoard) {
+		printGameBoard(gameBoard);
+		
+	}
+
+	public static void printGameBoard(char [] [] gameBoard) {
+		
+		for(char[] row : gameBoard) {
 			for(char c : row) {
 				System.out.print(c);
 			}
 			System.out.println();
 		}
-		
 	}
 
 }
