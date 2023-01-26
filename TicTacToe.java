704a4def2a99dd4e9da0f99bc59a2d010481bb55
Alexander Obregon <alexander.obregon47@gmail.com>
2023-01-25 16:24:08 -0800

    added playerPositions/cpuPositons and win condintion messages

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,12 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 public class TicTacToe {
+
+	static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+	static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
 
 	public static void main(String[] args) {
 
@@ -24,6 +30,8 @@ public class TicTacToe {
 			Random rand = new Random();
 			int cpuPos = rand.nextInt(9) +1;
 			placePiece(gameBoard, cpuPos, "cpu");
+
+			checkWinner();
 		}
 		
 		
@@ -72,6 +80,41 @@ public class TicTacToe {
 	printGameBoard(gameBoard);
 		
 		
+	}
+	public static String checkWinner() {
+		
+		List topRow = Arrays.asList(1, 2, 3);
+		List midRow = Arrays.asList(4, 5, 6);
+		List botRow = Arrays.asList(7, 8, 9);
+		List leftCol = Arrays.asList(1, 4, 7);
+		List midCol = Arrays.asList(2, 5, 8);
+		List rightCol = Arrays.asList(3, 6, 9);
+		List cross1 = Arrays.asList(1, 5, 9);
+		List cross2 = Arrays.asList(3, 5, 7);
+		
+		List<List> winning = new ArrayList<List>();
+		winning.add(topRow);
+		winning.add(midRow);
+		winning.add(botRow);
+		winning.add(leftCol);
+		winning.add(midCol);
+		winning.add(rightCol);
+		winning.add(cross1);
+		winning.add(cross2);
+		
+		for(List l : winning) {
+			if(playerPositions.containsAll(l)) {
+				return "You Win";
+			} else if(cpuPositions.contains(l)) {
+				return "CPU wins";
+			} else if(playerPositions.size() + cpuPositions.size() == 9) {
+				return "No one wins";
+			}
+		}
+		
+		
+		
+		return "";
 	}
 
 }
